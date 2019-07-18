244709e2cbb0dac30e3855e8f3e7eed1a7ebd07c
otmane <mastermajidosse@gmail.com>
2019-07-18 20:23:03 +0100

    first commit

---
+++ lib/costumicon.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CostumIcon {
+  static const IconData favorite = IconData(59518, fontFamily: 'MaterialIcons');
+  static const IconData search = IconData(59574, fontFamily: 'MaterialIcons');
+  static const IconData cart = IconData(59596, fontFamily: 'MaterialIcons');
+}
